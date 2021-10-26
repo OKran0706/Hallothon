b624f34c3f9b3139353c508cbc6413d29a84488e
Tanulr <73035034+Tanulr@users.noreply.github.com>
2021-10-27 00:18:19 +0530

    final hsv. Robot functionality

--- Hallothon/hsv.py
+++ Hallothon/hsv.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 import serial
 import pyzbar.pyzbar as pyzbar
 from datetime import datetime
 import time
-import speak
 ser = serial.Serial('COM4', 9600, timeout=1)
 move=0
+barcodeData=0
 from datetime import datetime
 morn = 0
 temp = 0
+cn=0
 b = 0
 detect = 7
 value = 0
@@ -19,25 +20,69 @@ t = 0
 import csv
 import receive
 import os
-import wakeup
+import speak
+from playsound import playsound
 
 while True:
     timenow = datetime.now()
     current_time = timenow.strftime("%H:%M:%S")
-    #print(current_time)
+    print(current_time[1])
     if current_time[1] == "7" and t == 0:
 
         move = 1
         detect=7
         morn=1
         t=1
-        wakeup.capture_face()
         #go to qr 1
     
     # server.qr = 0
     # if server(qr)
 
-      
+    if morn == 0:
+        
+        b = receive.getMessage()
+        print("morning",b)
+        if temp  != b:
+            value = b 
+            temp = b
+        print('value',value)
+        if value == 'mo':
+            print("mo")
+            os.system("robo.mp4")
+            value=0
+            move = 0
+        if value == 'j' and cn>0:
+            print('j')
+            cn=cn+1
+            #joke
+            speak.speak_text("What do you call a train carrying bubblegum? A chew chew train")
+            #smile detect in 10s ->sad/->nice smile
+            os.system("python smile.py")
+            value = 0
+            move = 0
+        if value == 'mu':
+            print("mu")
+            #music  
+            # for playing note.mp3 file
+            playsound('wake.mp3')
+            print('playing sound using  playsound')
+            value = 0
+            move = 0
+        
+        if str(value)=='1':
+            move=1
+            if barcodeData != "8":
+                #print('not 8')
+                move=1
+            if barcodeData == "8":
+                move=0
+        if str(value)=='2':
+            if barcodeData != "7":
+                move=1
+
+            if barcodeData == "7":
+                move=0
+    print('r')  
     _, frame = cap.read()
     barcodes = pyzbar.decode(frame)
     if len(barcodes)>0:
@@ -46,7 +91,6 @@ while True:
             barcodeData = barcode.data.decode('utf-8')
             barcodeType = barcode.type
             text = "{} ( {} )".format(barcodeData, barcodeType)
-            move=0
             print(barcodeData, morn)
             if morn == 1:
                 
@@ -55,63 +99,16 @@ while True:
                     ser.write(b'w')
                     ser.write(b'w')
                     os.system("python wakeup.py")
-                    detect=0
-                    exit()
+                    move=0
+                    morn =0
                 else:
                     ser.write(b'w')
                     print("left bar code")
                     ser.write(b'w')
-                    detect=0
                     move = 1
             
-    if morn == 0:
-        print("morning")
-        b = receive.getMessage()
-        if temp  != b:
-            value = b 
-            temp = b
-        if detect==barcodeData:
-
-        if value == 'mo':
-            print("mo")
-            os.system("robo.mp4")
-            value=0
-            move = 0
-        if value == 'j':
-            print('j')
-            speak.speak_text("Let me tell you a joke I know, your whole life hahaha")
-
-            #smile detect in 10s ->sad/->nice smile
-            os.system("python smile.py")
-            value = 0
-            move = 0
-        if value == 'mu':
-            print("mu")
-            #music
-            value = 0
-            move = 0
-        if value == "1":
-            detect= "8":
-            move = 1
-
-
-            move = 1
-            value = 0
-        if value == "2":
-            print("room")
-            move = 1
-            value = 0
-
-            ser.write(b'w')
-            ser.write(b'w')
-            time.sleep(1)
-            ser.write(b'w')
-            ser.write(b'w')
-                
-
- 
-
-    #action
+    print('l')
+    print('action')
     frame = cv2.blur(frame, (20,20))
     cv2.imshow("blred",frame)
     # Green color
@@ -136,7 +133,7 @@ while True:
         center=int(x),int(y)
         #print(int(x), int(y))
         #200,400 0
-
+        print(move)
         if move==1:
             if int(x)<150:
                 ser.write(b'a')
@@ -156,3 +153,8 @@ while True:
     if key == 27:
         ser.close()
         break
+    b = receive.getMessage()
+    if current_time[1] == "8":
+        os.system("python pose.py")
+    if current_time[1] == "9":
+        speak.speak_text("take your medicine")
